3f4333901a7521505660ca9ddf25b474d6976cf2
Krishna Anujan <ka2602@columbia.edu>
2025-02-27 15:18:45 -0500

    updates to make pages through code

--- files/makepage.R
+++ files/makepage.R
@@ -13,16 +13,39 @@ for (member in 1:nrow(member_list)) {
     writeLines(c(
         "---",
         paste0("title: \"", member_data$First.Name, " ", member_data$Last.Name, "\""),
-        # url
-        paste0("aliases: ", name, ".html"),
-        "---"
+        "---",
+        "",
+        "::::{.columns}",
+        ":::{.column}",
+        paste0("![", member_data$First.Name, " ", member_data$Last.Name, "](member_profiles/", member, ".png)"),
+        ":::",
+        ":::{.column}",
+        member_data$Institution, "<br>",
+        member_data$Email, "<br>",
+        ":::",
+        "::::"
     ), fileConn)
     close(fileConn)
+}
 
-    # add line to the index file
+# sort the member list by first name
+member_list <- member_list[order(member_list$First.Name), ]
+
+# write the index file
+index_page <- file("index.qmd", open = "w")
+writeLines(c(
+    "---",
+    paste0("title: Member directory"),
+    "---",
+    ""
+), index_page)
+close(index_page)
+
+for (i in 1:nrow(member_list)) {
+    name <- paste0(member_list[i, ]$First.Name, "_", member_list[i, ]$Last.Name)
     index_page <- file("index.qmd", open = "a")
     writeLines(c(
-        paste0("- [", member_data$First.Name, " ", member_data$Last.Name, "](./files/", name, ".html)")
+        paste0("- [", member_list[i, ]$First.Name, " ", member_list[i, ]$Last.Name, "](./files/", name, ".html)")
     ), index_page)
     close(index_page)
 }
